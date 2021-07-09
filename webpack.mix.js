df4d2e55e40aa53525d852bd9d093e8d7bba40cf
Fabio de Almeida Martins <fabio.martins.01@hotmail.com>
2021-07-09 17:59:01 -0300

    alterações 17h54

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,9 +9,35 @@ const mix = require('laravel-mix');
  | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
- */
-
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+ */
+
+mix 
+/*
+    .sass('node_modules/bootstrap/scss/bootstrap.scss','public/css/bootstrap.css')
+*/
+    .sass('resources/css/style.scss', 'public/css/style.css')
+    .scripts('node_modules/jquery/dist/jquery.js','public/js/jquery.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js','public/js/bootstrap.js') 
+    .scripts('resources/js/app.js', 'public/js/script.js')
+/*
+    .webpackConfig({
+        module: {
+        rules: [
+            {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/
+            }
+        ]
+        },
+        resolve: {
+        extensions: ["*", ".js", ".jsx", ".vue", ".ts", ".tsx"]
+        }
+    })*/
+;
+
